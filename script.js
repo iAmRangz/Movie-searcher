85d4a9e600e4e5526e904c6b45c0fe430e81f4f3
iAmRangz <superango2012@gmail.com>
2024-01-03 15:35:12 +0800

    Update getMovieHtml function

--- script.js
+++ script.js
@@ -11,14 +11,17 @@ const moviesContainer = document.getElementById('movies-container');
 
 // Build movie HTML
 function getMovieHtml(movie) {
-
     return `
-    <div class="movie">
-      <img src="http://image.tmdb.org/t/p/w500/${movie.poster_path}" alt="${movie.title}">
-      <h3>${movie.title}</h3>
-    </div>
-  `;
-
+        <div class="movie" 
+             data-id="${movie.id}"
+             data-title="${movie.title}"
+             data-poster-path="${movie.poster_path}"
+             data-release-date="${movie.release_date}"
+             data-overview="${movie.overview}">
+            <img src="http://image.tmdb.org/t/p/w500/${movie.poster_path}" alt="${movie.title}">
+            <h3>${movie.title}</h3>
+        </div>
+    `;
 }
 
 // Get upcoming movies on load
@@ -65,4 +68,71 @@ async function searchMovies(query) {
 searchBtn.addEventListener('click', () => {
     const searchQuery = searchInput.value;
     searchMovies(searchQuery);
+});
+
+
+function displayDetails(movie) {
+    axios
+        .get(`https://api.themoviedb.org/3/movie/${movie.id}?api_key=${apiKey}`)
+        .then(response => {
+            const movieDetails = response.data;
+
+            const detailsDiv = document.createElement('div');
+            detailsDiv.classList.add('details');
+
+            const closeButton = document.createElement('button');
+            closeButton.textContent = 'Close';
+            closeButton.addEventListener('click', () => {
+                document.body.removeChild(detailsDiv);
+            });
+
+            const similarMoviesButton = document.createElement('button');
+            similarMoviesButton.textContent = 'Get Similar Movies';
+            similarMoviesButton.addEventListener('click', async () => {
+                try {
+                    const similarResponse = await axios.get(`https://api.themoviedb.org/3/movie/${movie.id}/similar?api_key=${apiKey}`);
+                    const similarMovies = similarResponse.data.results;
+
+                    let similarMoviesHtml = '<h3>Similar Movies:</h3>';
+                    similarMovies.forEach(similarMovie => {
+                        similarMoviesHtml += getMovieHtml(similarMovie);
+                    });
+
+                    detailsDiv.innerHTML += similarMoviesHtml;
+                } catch (error) {
+                    console.log(error);
+                }
+            });
+
+            detailsDiv.innerHTML = `
+                <h2>${movieDetails.title}</h2>
+                <img src="http://image.tmdb.org/t/p/w500/${movieDetails.poster_path}" alt="${movieDetails.title}">
+                <p>Release Date: ${movieDetails.release_date}</p>
+                <p>Overview: ${movieDetails.overview}</p>
+            `;
+
+            detailsDiv.appendChild(closeButton);
+            detailsDiv.appendChild(similarMoviesButton);
+
+            document.body.appendChild(detailsDiv);
+        })
+        .catch(error => {
+            console.log(error);
+        });
+}
+
+
+
+moviesContainer.addEventListener('dblclick', event => {
+    const movieElement = event.target.closest('.movie');
+    if (movieElement) {
+        const movieData = {
+            id: movieElement.getAttribute('data-id'),
+            title: movieElement.getAttribute('data-title'),
+            poster_path: movieElement.getAttribute('data-poster-path'),
+            release_date: movieElement.getAttribute('data-release-date'),
+            overview: movieElement.getAttribute('data-overview')
+        };
+        displayDetails(movieData);
+    }
 });
